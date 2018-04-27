5772fb9694cc92bb26eeda1593c5c401459c4cf8
SedatD <dilmacsedat@gmail.com>
2018-04-27 14:59:32 +0300

    hero pojo

--- app/src/main/java/com/dilmacsedat/bestpickformobilelegendsbang/Activity/SplashActivity.java
+++ app/src/main/java/com/dilmacsedat/bestpickformobilelegendsbang/Activity/SplashActivity.java
@@ -1,11 +1,10 @@
 package com.dilmacsedat.bestpickformobilelegendsbang.Activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.dilmacsedat.bestpickformobilelegendsbang.DataModel.Hero;
 import com.dilmacsedat.bestpickformobilelegendsbang.R;
-import com.dilmacsedat.bestpickformobilelegendsbang.Util.Utils;
 
 import java.util.ArrayList;
 
@@ -19,10 +18,53 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         int i = 1;
-        heroArrayList.add(new Hero(i++,"123","123","qweqwe","asdasdasd"));
-        heroArrayList.add(new Hero(i++,"123","123","qweqwe","asdasdasd"));
-        heroArrayList.add(new Hero(i++,"123","123","qweqwe","asdasdasd"));
-        Utils.getHeroByType("asd");
 
+        //assassin
+        heroArrayList.add(new Hero(i++,"saber","Saber","assassin","loong story"));
+        heroArrayList.add(new Hero(i++,"karina","Karina","assassin","loong story"));
+        heroArrayList.add(new Hero(i++,"fanny","Fanny","assassin","loong story"));
+        heroArrayList.add(new Hero(i++,"hayabusa","Hayabusa","assassin","loong story"));
+        heroArrayList.add(new Hero(i++,"natalia","Natalia","assassin","loong story"));
+        heroArrayList.add(new Hero(i++,"lancelot","Lancelot","assassin","loong story"));
+        heroArrayList.add(new Hero(i++,"helcurt","Helcurt","assassin","loong story"));
+        //heroArrayList.add(new Hero(i++,"gusion","Gusion","assassin","loong story"));
+
+        //tank
+        heroArrayList.add(new Hero(i++,"tigreal","Tigreal","tank","loong story"));
+        heroArrayList.add(new Hero(i++,"akai","Akai","tank","loong story"));
+        heroArrayList.add(new Hero(i++,"franco","Franco","tank","loong story"));
+        heroArrayList.add(new Hero(i++,"minotaur","Minotaur","tank","loong story"));
+        heroArrayList.add(new Hero(i++,"lolita","Lolita","tank","loong story"));
+        heroArrayList.add(new Hero(i++,"johnson","Johnson","tank","loong story"));
+        heroArrayList.add(new Hero(i++,"gatotkaca","Gatotkaca","tank","loong story"));
+        heroArrayList.add(new Hero(i++,"grock","Grock","tank","loong story"));
+        heroArrayList.add(new Hero(i++,"hylos","Hylos","tank","loong story"));
+
+        //fighter
+        heroArrayList.add(new Hero(i++,"saber","Saber","fighter","loong story"));
+
+        //mage
+        heroArrayList.add(new Hero(i++,"saber","Saber","mage","loong story"));
+
+        //marksman
+        heroArrayList.add(new Hero(i++,"miya","Miya","marksman","loong story"));
+        heroArrayList.add(new Hero(i++,"bruno","Bruno","marksman","loong story"));
+        heroArrayList.add(new Hero(i++,"clint","Clint","marksman","loong story"));
+        heroArrayList.add(new Hero(i++,"layla","Layla","marksman","loong story"));
+        heroArrayList.add(new Hero(i++,"yisunshin","Yi Sun-shin","marksman","loong story"));
+        heroArrayList.add(new Hero(i++,"moskov","Moskov","marksman","loong story"));
+        heroArrayList.add(new Hero(i++,"karrie","Karrie","marksman","loong story"));
+        heroArrayList.add(new Hero(i++,"irithel","Irithel","marksman","loong story"));
+        heroArrayList.add(new Hero(i++,"lesley","Lesley","marksman","loong story"));
+        //heroArrayList.add(new Hero(i++,"hanabi","Hanabi","marksman","loong story"));
+
+        //support
+        heroArrayList.add(new Hero(i++,"rafaela","Rafaela","support","loong story"));
+        heroArrayList.add(new Hero(i++,"estes","Estes","support","loong story"));
+        heroArrayList.add(new Hero(i++,"diggie","Diggie","support","loong story"));
+        //heroArrayList.add(new Hero(i++,"angela","Angela","support","loong story"));
+
+        //Utils.getHeroByType("asd");
     }
+
 }
