716695e3cf528ca81bc06f3fc90e2e4110ee2e85
SedatD <dilmacsedat@gmail.com>
2018-04-27 14:07:15 +0300

    hero pojo

--- app/src/main/java/com/dilmacsedat/bestpickformobilelegendsbang/Activity/SplashActivity.java
+++ app/src/main/java/com/dilmacsedat/bestpickformobilelegendsbang/Activity/SplashActivity.java
@@ -3,14 +3,26 @@ package com.dilmacsedat.bestpickformobilelegendsbang.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.dilmacsedat.bestpickformobilelegendsbang.DataModel.Hero;
 import com.dilmacsedat.bestpickformobilelegendsbang.R;
+import com.dilmacsedat.bestpickformobilelegendsbang.Util.Utils;
+
+import java.util.ArrayList;
 
 public class SplashActivity extends AppCompatActivity {
     private static final String TAG = "SplashAct";
+    public static ArrayList<Hero> heroArrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        int i = 1;
+        heroArrayList.add(new Hero(i++,"123","123","qweqwe","asdasdasd"));
+        heroArrayList.add(new Hero(i++,"123","123","qweqwe","asdasdasd"));
+        heroArrayList.add(new Hero(i++,"123","123","qweqwe","asdasdasd"));
+        Utils.getHeroByType("asd");
+
     }
 }
